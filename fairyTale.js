589fa3ae083e517b85863300b721f44a02934c77
kiaras111 <149087851+kiaras111@users.noreply.github.com>
2024-01-30 16:25:05 +0000

    let varibles

--- fairyTale.js
+++ fairyTale.js
@@ -1,27 +1,27 @@
 // 1. Create a variable named `place` and set the value to a string that says whatever place you want.
-
+let place = "The Moon";
 // 2. Create a variable named `creatureName` and set the value to a string that says whatever name you want to give the creature.
-
+let creatureName = "Wyvren";
 // 3. Create a variable named `color` and set the value to a string that says whatever color you want.
-
+let color = "red";
 // 4. Create a variable named `color2` and set the value to a string that says a different color you want.
-
+let color2 = "blue";
 // 5. Create a variable named `emotion` and set the value to a string that says whatever emotion you want.
-
+let emotion = "happy";
 // 6. Create a variable named `character` and set the value to a string that says whatever name for a character you want.
-
+let character = "Tova";
 // 7. Create a variable named `number` and set the value to a number greater than 1.
-
+let number = "2";
 // 8. Create a variable named `item` and set the value to a string that says whatever item/thing you want.
-
+let item = "Sword";
 // 9. Create a variable named `item2` and set the value to a string that says a different item/thing you want.
-
+let item2 = "Shield";
 // 10. Create a variable named `verb` and set the value to a string that says whatever verb you want.
-
+let verb = "slay";
 // 11. Create a variable named `adverb` and set the value to a string that says whatever adverb you want.
-
+let adverb = "quickly";
 // 12. Create a variable named `number2` and set the value to a number greater than 1.
-
+let number2 = "3";
 
 
 
